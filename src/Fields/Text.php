3c0b508736bc80c101e07f031b1a9f87bc46894c
Mohammad Emran <phpfour@gmail.com>
2022-11-20 08:54:20 +0600

    Add limit support for text and textarea columns.

--- src/Fields/Text.php
+++ src/Fields/Text.php
@@ -5,4 +5,14 @@ namespace FigLab\CrudResource\Fields;
 final class Text extends Field
 {
     protected string $type = 'text';
+
+    /**
+     * Character limit in column display, default is 50.
+     */
+    public function limit(int $limit): self
+    {
+        $this->props['limit'] = $limit;
+
+        return $this;
+    }
 }
--- src/Fields/Textarea.php
+++ src/Fields/Textarea.php
@@ -5,4 +5,14 @@ namespace FigLab\CrudResource\Fields;
 final class Textarea extends Field
 {
     protected string $type = 'textarea';
+
+    /**
+     * Character limit in column display, default is 50.
+     */
+    public function limit(int $limit): self
+    {
+        $this->props['limit'] = $limit;
+
+        return $this;
+    }
 }
