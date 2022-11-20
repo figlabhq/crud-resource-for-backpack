496001537a27efbefd3cc770f1ba7ebdda832618
Mohammad Emran <phpfour@gmail.com>
2022-11-20 08:02:23 +0600

    Add support for custom fields. Closes #1.

---
+++ src/Fields/Custom.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace FigLab\CrudResource\Fields;
+
+final class Custom extends Field
+{
+    /**
+     * The custom field type.
+     */
+    public function type(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Any additional options for the custom field, will be merged with props.
+     */
+    public function options(array $options = []): self
+    {
+        $this->props = array_merge($this->props, $options);
+
+        return $this;
+    }
+}
